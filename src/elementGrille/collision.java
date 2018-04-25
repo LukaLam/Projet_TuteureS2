0c3304c4db911d0587a73d5df7a540a96d2d11e6
VirgileMezz <virgile.mezzarobba@laposte.net>
2018-04-25 22:03:58 +0200

    bonjour j'ai mis un commentaire

--- src/elementGrille/collision.java
+++ src/elementGrille/collision.java
@@ -3,5 +3,6 @@ package elementGrille;
 public class collision extends Mur {
     public boolean heurter(){
         //On verifie ici si il y'a collision entre le snake et un mur, ou entre la tête du snake et lui même
+        //test vivi git
     }
 }
